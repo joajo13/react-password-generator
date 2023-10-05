9bd4b47afa4734e67971b3548de30b8d73401e44
juan <juanbejamingiupponi@gmail.com>
2023-10-05 11:37:51 -0300

    Responsive mejorado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = React.memo(() => {
   return (
-    <footer className="bg-white rounded-lg shadow-xl m-4 dark:bg-gray-900 lg:absolute bottom-0 left-0">
+    <footer className=" rounded-lg shadow-xl m-4 dark:bg-gray-900 lg:absolute bottom-0 left-0">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
--- src/components/PasswordCard.jsx
+++ src/components/PasswordCard.jsx
@@ -5,8 +5,8 @@ export const PasswordCard = React.memo(
   ({ copyToClipboard, handleClick, password }) => {
     return (
       <div className="flex justify-center mt-10">
-        <div className="box rounded-full max-w-full w-full mx-4 md:w-2/4 md:mx-auto mt-6 mb-6 inline-flex justify-between h-14 items-center dark:bg-slate-700">
-          <button className="flex hover:bg-emerald-600 hover:bg-opacity-40 rounded-2xl p-2 ml-2 h-10 w-10 items-center font-semibold active:scale-90 duration-75">
+        <div className="box text-sm md:text-lg rounded-full max-w-sm w-full mx-4 md:max-w-full md:w-2/4 md:mx-auto mt-6 mb-6 inline-flex justify-between h-14 items-center dark:bg-slate-700">
+          <button className="flex bg-emerald-600 bg-opacity-40 md:bg-transparent hover:bg-emerald-600 hover:bg-opacity-40 rounded-2xl p-2 ml-2 h-10 w-10 items-center font-semibold active:scale-90 duration-75">
             <BiCopyAlt
               size={32}
               onClick={() => copyToClipboard(password)}
@@ -18,7 +18,7 @@ export const PasswordCard = React.memo(
           </p>
           <button
             onClick={handleClick}
-            className="flex hover:bg-sky-600 font-semibold active:scale-90 duration-75 hover:bg-opacity-40 dark:text-white rounded-2xl p-2 mr-2 items-center"
+            className="flex bg-sky-600 bg-opacity-40 h-10 md:bg-transparent hover:bg-sky-600 font-semibold active:scale-90 duration-75 hover:bg-opacity-40 dark:text-white rounded-2xl p-2 mr-2 items-center"
           >
             Generar
           </button>
